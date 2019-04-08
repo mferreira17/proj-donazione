63af28d80c47f1786c72ae4ec99c266e0b1958c8
7950 <marciow.ferreira@bancoamazonia.com.br>
2019-04-08 14:46:19 -0300

    ajustes

--- src/main/java/br/com/donazo/donazione/DonazioneApplication.java
+++ src/main/java/br/com/donazo/donazione/DonazioneApplication.java
@@ -2,7 +2,7 @@ package br.com.donazo.donazione;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class DonazioneApplication {
@@ -10,5 +10,4 @@ public class DonazioneApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(DonazioneApplication.class, args);
 	}
-
 }
