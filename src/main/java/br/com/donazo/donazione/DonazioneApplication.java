e4d9ba799c3986890ad82786a43836bd31f7602d
suporte <suporte@INF104>
2019-04-12 09:59:58 -0300

    Correção no menu/paginas e inserção do crud de ações.

--- src/main/java/br/com/donazo/donazione/DonazioneApplication.java
+++ src/main/java/br/com/donazo/donazione/DonazioneApplication.java
@@ -2,12 +2,12 @@ package br.com.donazo.donazione;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class DonazioneApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(DonazioneApplication.class, args);
-	}
+    public static void main(final String[] args) {
+
+        SpringApplication.run(DonazioneApplication.class, args);
+    }
 }
