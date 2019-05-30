8fee76db984dc364c0f7dd2269786fb0164b73d1
7950 <marciow.ferreira@bancoamazonia.com.br>
2019-05-30 13:44:13 -0300

    ajustes locais

--- src/main/java/br/com/donazo/donazione/PagSeguroBean.java
+++ src/main/java/br/com/donazo/donazione/PagSeguroBean.java
@@ -4,9 +4,9 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
-import br.com.uol.pagseguro.api.PagSeguro;
+/*import br.com.uol.pagseguro.api.PagSeguro;
 import br.com.uol.pagseguro.api.PagSeguroEnv;
-import br.com.uol.pagseguro.api.credential.Credential;
+import br.com.uol.pagseguro.api.credential.Credential;*/
 
 @Configuration
 public class PagSeguroBean {
@@ -23,11 +23,10 @@ public class PagSeguroBean {
 	@Value(value = "${pagseguro.token}")
 	private String token;
 
-	@Bean
-	public PagSeguro createPagSeguroInstance() {
-		Credential credentials = Credential.sellerCredential(email, token);
-		PagSeguro pagseguroInstance = PagSeguro.instance(credentials, PagSeguroEnv.PRODUCTION);
-		return pagseguroInstance;
-	}
-
+	/*
+	 * @Bean public PagSeguro createPagSeguroInstance() { Credential credentials =
+	 * Credential.sellerCredential(email, token); PagSeguro pagseguroInstance =
+	 * PagSeguro.instance(credentials, PagSeguroEnv.PRODUCTION); return
+	 * pagseguroInstance; }
+	 */
 }
