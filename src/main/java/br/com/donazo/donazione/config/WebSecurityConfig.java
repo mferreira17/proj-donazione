f1ae9fcc628ed952cc11edc9369401784987c406
Márcio Ferreira <mwferreira17@gmail.com>
2019-05-31 00:42:45 -0300

    ajustes

--- src/main/java/br/com/donazo/donazione/config/WebSecurityConfig.java
+++ src/main/java/br/com/donazo/donazione/config/WebSecurityConfig.java
@@ -3,45 +3,37 @@ package br.com.donazo.donazione.config;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
 import br.com.donazo.donazione.seguranca.DonazioneUserDetailsService;
 
 @Configuration
+@EnableWebSecurity
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
-	
+
 	@Autowired
 	private DonazioneUserDetailsService userDetailsService;
-	
+
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
-		http.
-			authorizeRequests()
-				.antMatchers("/javax.faces.resource/**", "/publica/**").permitAll()
+
+		http.authorizeRequests().antMatchers("/javax.faces.resource/**", "/publica/**").permitAll()
 				.antMatchers("/autenticado/pages/campanha/**").hasAnyRole("ADMINISTRADOR")
 				.antMatchers("/autenticado/pages/colaborador/**").hasAnyRole("ADMINISTRADOR")
 				.antMatchers("/autenticado/pages/itemdoacao/**").hasAnyRole("ADMINISTRADOR")
 				.antMatchers("/autenticado/pages/evento/lista/**").hasAnyRole("COLABORADOR", "ADMINISTRADOR")
 				.antMatchers("/autenticado/pages/evento/**").hasAnyRole("ADMINISTRADOR")
 				.antMatchers("/autenticado/pages/doacao/**").hasAnyRole("ADMINISTRADOR", "COLABORADOR")
-				.antMatchers("/autenticado").hasAnyRole("ADMINISTRADOR", "COLABORADOR")
-				.anyRequest()
-				.authenticated()
-			.and()
-			.formLogin()
-				.loginPage("/index.xhtml")
-				.failureUrl("/index.xhtml?error=true")
-				.permitAll().successForwardUrl("/autenticado/dashboard.xhtml")
-				.defaultSuccessUrl("/autenticado/dashboard.xhtml", true)
-			.and()
-			.logout()
-				.logoutSuccessUrl("/index.xhtml?logout=true")
-				.permitAll()
-			.and()
-			.rememberMe()
+				.antMatchers("/autenticado").hasAnyRole("ADMINISTRADOR", "COLABORADOR").anyRequest().authenticated()
+				.and().formLogin().loginPage("/index.xhtml").failureUrl("/index.xhtml?error=true").permitAll()
+				.successForwardUrl("/autenticado/dashboard.xhtml")
+				.defaultSuccessUrl("/autenticado/dashboard.xhtml", true).and().logout()
+				.logoutSuccessUrl("/index.xhtml?logout=true").permitAll().and().rememberMe()
 				.userDetailsService(userDetailsService);
-		
+
 		http.csrf().disable();
+
 	}
 
 }
